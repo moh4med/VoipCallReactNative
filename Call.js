be11219dca7d83eef76a57205aba009979b5ef39
moh4med <moh4med.ahmed@gmail.com>
2018-12-01 13:54:52 +0200

    integrate with socket io

--- Call.js
+++ Call.js
@@ -20,25 +20,58 @@ type Props = {};
 const Width = Dimensions.get('window').width
 const Height = Dimensions.get('window').height
 import RingToneModule from './modules'
+import SocketIOClient from 'socket.io-client';
+const serverUrl = "http://192.168.1.5:3000"
 export default class Call extends Component<Props> {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      textMsg:"New Call arrived",
-      newCall:true
+    this.state = {
+      textMsg: "New Call arrived",
+      newCall: true
     }
   }
-  componentDidMount(){
-    RingToneModule.PlaySound()
+  componentDidMount() {
+    // RingToneModule.PlaySound()
+  }
+  ConnectSocket = () => {
+    try {
+
+      this.socket = SocketIOClient(serverUrl);
+      this.socket.on('channel2', (data) => {
+        try {
+          alert(data)
+          console.log("TAG", 'Data recieved from server', data); //this will console 'channel 2'        
+        } catch (error) {
+          console.log("TAG", "error", error)
+        }
+      });
+      this.socket.on('error', function onSocketError(e) {
+        console.log('TAG', 'WebSocket Error ' + e);
+      });
+    } catch (error) {
+      console.log("TAG", "Error", error)
     }
-    handleRefuse=()=>{
-      RingToneModule.StopSound()
-      this.setState({textMsg:"Call Refused",newCall:false})
+
+  }
+  sendData = () => {
+    try {
+      console.log("TAG", "Sending data ")
+      this.socket.emit('channel3', 'Hello server')
+    } catch (error) {
+      console.log("TAG", "Error", error)
     }
-    handleAccepet=()=>{
-      RingToneModule.StopSound()
-      this.setState({textMsg:"Call accepted",newCall:false})
-    }
+  }
+  closeSocket = () => {
+    this.socket.disconnect()
+  }
+  handleRefuse = () => {
+    RingToneModule.StopSound()
+    this.setState({ textMsg: "Call Refused", newCall: false })
+  }
+  handleAccepet = () => {
+    RingToneModule.StopSound()
+    this.setState({ textMsg: "Call accepted", newCall: false })
+  }
 
   render() {
     return (
@@ -48,16 +81,21 @@ export default class Call extends Component<Props> {
         style={styles.container}>
         <Text style={styles.newcallText}>{this.state.textMsg} </Text>
 
-        {this.state.newCall&&<View style={styles.buttonsContainer}>
-          <TouchableOpacity 
-          onPress={this.handleRefuse}
-          style={[styles.btn, { backgroundColor: "red" }]}>
-            <Text style={[styles.newcallText,{fontSize:14}]}>refuse</Text>
+        {this.state.newCall && <View style={styles.buttonsContainer}>
+          <TouchableOpacity
+            onPress={this.closeSocket}
+            style={[styles.btn, { backgroundColor: "red" }]}>
+            <Text style={[styles.newcallText, { fontSize: 14 }]}>disconnect</Text>
           </TouchableOpacity>
-          <TouchableOpacity 
-           onPress={this.handleAccepet}
-          style={[styles.btn, { backgroundColor: "green" }]}>
-            <Text style={[styles.newcallText,{fontSize:14}]}>accept</Text>
+          <TouchableOpacity
+            onPress={this.ConnectSocket}
+            style={[styles.btn, { backgroundColor: "blue" }]}>
+            <Text style={[styles.newcallText, { fontSize: 14 }]}>connect</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.sendData}
+            style={[styles.btn, { backgroundColor: "green" }]}>
+            <Text style={[styles.newcallText, { fontSize: 14 }]}>send data</Text>
           </TouchableOpacity>
         </View>}
       </ImageBackground>
@@ -75,8 +113,8 @@ const styles = StyleSheet.create({
     width: Width * .2,
     height: Width * .2,
     borderRadius: Width * .1,
-    justifyContent:"center",
-    alignItems:"center"
+    justifyContent: "center",
+    alignItems: "center"
   },
   buttonsContainer: {
     marginTop: Height * .2,
--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName,callScreenname} from './app.json';
 import Call from './Call';
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Call);
 AppRegistry.registerComponent(callScreenname, () => Call);
 
